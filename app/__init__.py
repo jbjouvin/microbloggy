a0ab9d18bbbaa3fdea9f82e6f2479e17def5fe43
Jean-Baptiste Jouvin <jb.jouvin@gmail.com>
2018-02-25 22:11:32 +0100

    @user registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
+# Softbank robotics documentation pepper proxim'it
 appy = Flask(__name__)
-bootstrap = Bootstrap(appy)
 appy.config.from_object(Config)
 
-from app import routes
+bootstrap = Bootstrap(appy)
+
+db = SQLAlchemy(appy)
+Migrate = Migrate(appy, db)
+
+Login = LoginManager(appy)
+Login.login_view = "login"
+
+from app import routes, models
--- microbloggy.py
+++ microbloggy.py
@@ -1,4 +1,9 @@
-from app import appy
+from app import appy, db
+from app.models import User, Post
+
+@appy.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 if __name__ == '__main__':
     appy.run()
