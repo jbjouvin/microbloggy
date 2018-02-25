22d192d95cabea75949b633bebb2bf22e9b64175
Jean-Baptiste Jouvin <jb.jouvin@criteo.com>
2018-02-25 14:21:24 +0100

    + bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
 
 appy = Flask(__name__)
 bootstrap = Bootstrap(appy)
+appy.config.from_object(Config)
 
 from app import routes
