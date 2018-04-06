cc4a042b5be5dde58fa3b0915ec1d6e5fbb58a3a
jbjouvin <jb.jouvin@gmail.com>
2018-04-06 22:46:02 +0200

    + pagination

--- app/routes.py
+++ app/routes.py
@@ -14,8 +14,8 @@ def before_request():
         db.session.commit()
 
 
-@appy.route('/', methods=['GET','POST'])
-@appy.route('/index', methods=['GET','POST'])
+@appy.route('/', methods=['GET', 'POST'])
+@appy.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     # user = {'username': 'Miguel'}
@@ -27,8 +27,15 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home', posts=posts, form=form)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, appy.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', posts=posts.items, form=form, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @appy.route('/login', methods=['GET', 'POST'])
@@ -81,11 +88,15 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, appy.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @appy.route("/edit_profile", methods=['GET', 'POST'])
@@ -140,5 +151,12 @@ def unfollow(username):
 @appy.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, appy.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
